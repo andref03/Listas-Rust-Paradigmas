36c9c9e797a91dea01e1f5334dbd5d83879c3e5a
Gustavo dos Santos Leão <102435052+agatrix@users.noreply.github.com>
2025-06-14 20:02:20 -0300

    add Ordem Crescente

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
-fn executar_estrategia(mut lista: Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
-    estrategia(&mut lista);
+fn ordemCrescente(lista: &mut Vec<i64>) {
+    let n = lista.len();
+    for i in 1..n {
+        let mut j = i;
+        while j > 0 && lista[j - 1] > lista[j] {
+            lista.swap(j, j - 1);
+            j -= 1;
+        }
+    }
+}
+
+fn executar_estrategia(lista: &mut Vec<i64>, estrategia: fn(&mut Vec<i64>)) {
+    estrategia(lista);
     println!("Resultado: {:?}", lista);
 }
 
 fn main() {
-    let numeros = vec![5, 2, 8, 5, 2, 9, 1, 0, 4, 4];
+    let mut numeros : Vec<i64> = vec![5, 2, 8, 5, 2, 9, 1, 0, 4, 4];
     println!("Lista original: {:?}", numeros);
+    executar_estrategia(&mut numeros,ordemCrescente);
+    
 
 }
